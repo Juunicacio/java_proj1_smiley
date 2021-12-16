e495c2084bd5b283b4b097a068fd8254a8e17a64
Juunicacio <juhnicacio.bio@gmail.com>
2021-12-16 19:00:09 +0100

    added a Graphics2D to draw a larger black stroke to the border of the circle

--- src/java_proj1_drawSmiley/MyPanelPaint.java
+++ src/java_proj1_drawSmiley/MyPanelPaint.java
@@ -1,17 +1,23 @@
 package java_proj1_drawSmiley;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 public class MyPanelPaint extends JPanel{
 	private static final long serialVersionUID = 1L;
 	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
 		// draw circle
-		g.fillOval(100, 100, 50, 50);
+		// stroke draw of the circle
+		g2.setStroke(new BasicStroke(2));
 		g.setColor(Color.black);
 		g.drawOval(100, 100, 50, 50);
+		g.setColor(Color.yellow);
+		g.fillOval(100, 100, 50, 50);
 		System.out.println("just got executed!");
 	}
 }
