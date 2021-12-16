1267dbcf1f676a81f47b728c88929b1573306996
Juunicacio <juhnicacio.bio@gmail.com>
2021-12-16 21:17:26 +0100

    factoring

--- src/java_proj1_drawSmiley/MyPanelPaint.java
+++ src/java_proj1_drawSmiley/MyPanelPaint.java
@@ -12,29 +12,37 @@ public class MyPanelPaint extends JPanel{
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		// draw circle
+		drawOutline(g, g2);
+		drawMouth(g,g2);
+		drawEyes(g);
+	}
+	
+	static void drawOutline(Graphics g, Graphics2D g2) {
 		// stroke draw of the circle
 		g2.setStroke(new BasicStroke(2));
 		g.setColor(Color.black);
 		g.drawOval(100, 100, 50, 50);
 		g.setColor(Color.yellow);
 		g.fillOval(100, 100, 50, 50);
-		System.out.println("just got executed!");
-		
+		System.out.println("just got executed!");				
+	}
+	static void drawMouth(Graphics g, Graphics2D g2) {
 		// draw the mouth
 		g2.setStroke(new BasicStroke(1));
 		g.setColor(Color.black);
 		g.drawOval(110, 110, 30, 30);
-		
+		// rectangle to cover circle of mouth
 		g2.setStroke(new BasicStroke(2));
 		g.setColor(Color.yellow);
-		//g.drawRect(107, 110, 36, 20);
-		g.fillRect(107, 110, 36, 20);
-		
-		
+		g.fillRect(107, 110, 36, 20);		
+	}
+	static void drawEyes(Graphics g) {
 		// draw the eyes
 		g.setColor(Color.black);
 		g.fillOval(115,115,5,5);		
 		g.setColor(Color.black);
-		g.fillOval(130,115,5,5);
+		g.fillOval(130,115,5,5);		
 	}
+	
+	
 }
